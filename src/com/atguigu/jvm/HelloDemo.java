3f05c92299e9ac9298a11436cfe5e2ad0630d849
2020-11-01 <2367127566@qq.com>
2021-08-30 18:37:43 +0800

    测试文件添加

---
+++ src/com/atguigu/jvm/HelloDemo.java
@@ -0,0 +1,15 @@
+package com.atguigu.jvm;
+
+/**
+ * <p>简单描述该类的作用</p>
+ *
+ * @author xuqinghui
+ * @create 2021/8/30 18:34
+ */
+public class HelloDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello demo test");
+    }
+
+}
